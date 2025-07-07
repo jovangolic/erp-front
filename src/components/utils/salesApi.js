970a84567cd58c2c298bc7fcbd5f58b13636b455
jovan <jovangolic19@gmail.com>
2025-07-07 19:26:25 +0200

    committed

--- src/components/utils/salesApi.js
+++ src/components/utils/salesApi.js
@@ -103,23 +103,6 @@ export async function getByCreatedAtBetween({startDate, endDate}){
     }
 }
 
-export async function getSalesByBuyer(buyerId){
-    try{
-        if(!buyerId){
-            throw new Error("Dati buyerId nije pronadjen");
-        }
-        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/get-by-buyer`,{
-            params:{
-                buyerId:buyerId
-            },
-            headers:getHeader()
-        });
-        return response.data;
-    }
-    catch(error){
-        handleApiError(error, "Greska prilikom trazanje po kupcu");
-    }
-}
 
 export async function getByTotalPrice(totalPrice){
     try{
@@ -183,6 +166,188 @@ export async function getAllSales(){
         handleApiError(error,"Greksa prilikom dobavljanja svih prodaja");
     }
 }
+
+export async function findByBuyer_Id(buyerId){
+    try{
+        if(buyerId == null || isNaN(buyerId)){
+            throw new Error("Dati ID za kupca nije pronadjen");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/buyer/${buyerId}`,{
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po ID-iju kupca");
+    }
+}
+
+export async function findByBuyer_CompanyNameContainingIgnoreCase(buyerCompanyName){
+    try{
+        if(!buyerCompanyName || typeof buyerCompanyName !== "string" || buyerCompanyName.trim() === ""){
+            throw new Error("Dati naziv kupceve kompanije nije pronadjen");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-company-name`,{
+            params:{buyerCompanyName:buyerCompanyName},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po nazivu kupceve kompanije");
+    }
+}
+
+export async function findByBuyer_PibContainingIgnoreCase(buyerPib){
+    try{
+        if(!buyerPib || typeof buyerPib !== "string" || buyerPib.trim() === ""){
+            throw new Error("Dati PIB za kupca nije pronadjen");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-pib`,{
+            params:{buyerPib:buyerPib},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po PIB-u kupca");
+    }
+}
+
+export async function findByBuyer_AddressContainingIgnoreCase(buyerAddress){
+    try{
+        if(!buyerAddress || typeof buyerAddress !== "string" || buyerAddress.trim() === ""){
+            throw new Error("Data adresa kupca nije pronadjena");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-address`,{
+            params:{buyerAddress:buyerAddress},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja adrese kupca");
+    }
+}
+
+export async function findByBuyer_ContactPerson(contactPerson){
+    try{
+        if(!contactPerson || typeof contactPerson !== "string" || contactPerson.trim() === ""){
+            throw new Error("Data kontakt-osoba nije pronadjena");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-contact-person`,{
+            params:{contactPerson:contactPerson},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po kontakt-osobi");
+    }
+}
+
+export async function findByBuyer_EmailContainingIgnoreCase(buyerEmail){
+    try{
+        if(!buyerEmail || typeof buyerEmail !== "string" || buyerEmail.trim() === ""){
+            throw new Error("Dati email kupca nije pronadjen");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-email`,{
+            params:{buyerEmail:buyerEmail},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po emailu kupca");
+    }
+}
+
+export async function findByBuyer_PhoneNumber(buyerPhoneNumber){
+    try{
+        if(!buyerPhoneNumber || typeof buyerPhoneNumber !== "string" || buyerPhoneNumber.trim() === ""){
+            throw new Error("Dati broj telefona kupca nije pronadjen");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-phone-number`,{
+            params:{buyerPhoneNumber:buyerPhoneNumber},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po broju telefona kupca");
+    }
+}
+
+export async function findByTotalPriceGreaterThan(totalPrice){
+    try{    
+        const parseTotalPrice = parseFloat(totalPrice);
+        if(isNaN(parseTotalPrice) || parseTotalPrice <= 0){
+            throw new Error("Data ukupna cena nije pronadjena");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-total-price-max`,{
+            params:{totalPrice:parseTotalPrice},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po ukupnoj ceni vecoj od");
+    }
+}
+
+export async function findByTotalPriceLessThan(totalPrice){
+    try{    
+        const parseTotalPrice = parseFloat(totalPrice);
+        if(isNaN(parseTotalPrice) || parseTotalPrice <= 0){
+            throw new Error("Data ukupna cena nije pronadjena");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search/by-total-price-min`,{
+            params:{totalPrice:parseTotalPrice},
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom trazenja po ukupnoj ceni manjoj od");
+    }
+}
+
+export async function searchSales({buyerId, companyName, pib, email, phoneNumber, address, contactPerson, minTotalPrice, maxTotalPrice}){
+    try{
+        const parseMinTotalPrice = parseFloat(minTotalPrice);
+        const parseMaxTotalPrice = parseFloat(maxTotalPrice);
+        if(
+            buyerId == null || isNaN(buyerId) ||
+            !companyName || typeof companyName !== "string" || companyName.trim() === "" ||
+            !pib || typeof pib !== "string" || pib.trim() === "" ||
+            !email || typeof email !== "string" || email.trim() === "" ||
+            !phoneNumber || typeof phoneNumber !== "string" || phoneNumber.trim() === "" ||
+            !address || typeof address !== "string" || address.trim() === "" ||
+            !contactPerson || typeof contactPerson !== "string" || contactPerson.trim() === "" ||
+            isNaN(parseMinTotalPrice) || parseMinTotalPrice <= 0 || isNaN(parseMaxTotalPrice) || parseMaxTotalPrice <= 0
+        ){
+            throw new Error("Dati parametri za pretragu nisu validni");
+        }
+        const response = await api.get(`${import.meta.env.VITE_API_BASE_URL}/sales/search`,{
+            params:{
+                buyerId:buyerId,
+                companyName:companyName,
+                pib:pib,
+                phoneNumber:phoneNumber,
+                address:address,
+                contactPerson:contactPerson,
+                minTotalPrice: parseMinTotalPrice,
+                maxTotalPrice: parseMaxTotalPrice
+            },
+            headers:getHeader()
+        });
+        return response.data;
+    }
+    catch(error){
+        handleApiError(error,"Greska prilikom pretrage po parametrima");
+    }
+}
+
 
 function handleApiError(error, customMessage) {
     if (error.response && error.response.data) {

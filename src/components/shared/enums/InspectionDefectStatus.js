021ff156a11d8f0018e100f3d839a83dad2f02a9
jovan <jovangolic19@gmail.com>
2025-10-25 22:46:36 +0200

    committed

---
+++ src/components/shared/enums/InspectionDefectStatus.js
@@ -0,0 +1,8 @@
+export const InspectionDefectStatus = Object.freeze({
+
+    ACTIVE : "ACTIVE",
+	NEW : "NEW",
+    CONFIRMED : "CONFIRMED",
+    CLOSED : "CLOSED",
+    CANCELLED : "CANCELLED"
+});
